546f64e02d13909077e6d966a989e6b847191785
Max Bronckers <maxbronckers@me.com>
2022-03-16 22:46:58 +0000

    Plots

--- jupyter/ece_loss.py
+++ jupyter/ece_loss.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 source: https://gist.github.com/gpleiss/0b17bc4bd118b49050056cfcd5446c71
 """
 
-def make_model_diagrams(probabilities, predictions, confidences, labels, n_bins=10):
+def make_model_diagrams(probabilities, predictions, confidences, labels, model, n_bins=10):
     """
     outputs - a torch tensor (size n x num_classes) with the outputs from the final linear layer
     - NOT the softmaxes
@@ -31,23 +31,23 @@ def make_model_diagrams(probabilities, predictions, confidences, labels, n_bins=
     print(bin_corrects)
     print(type(bin_corrects))
 
-    fig, ax = plt.figure(0, figsize=(8, 8))
+    fig, ax = plt.subplots(1, 1, figsize=(8,8))
     
     confs = plt.bar(bin_centers, bin_corrects, color=[0, 0, 1], width=width, ec='black')
     # bin_corrects = np.nan_to_num(np.array([bin_correct.cpu().numpy()  for bin_correct in bin_corrects]))
     # gaps = plt.bar(bin_centers, gap, bottom=bin_corrects, color=[1, 0.7, 0.7], alpha=0.5, width=width, hatch='//', edgecolor='r')
     
-    plt.plot([0, 1], [0, 1], '--', color='gray')
-    plt.legend([confs], ['Accuracy'], loc='upper left', fontsize='x-large')
+    ax.plot([0, 1], [0, 1], '--', color='gray')
+    ax.legend([confs], ['Accuracy'], loc='upper left', fontsize='x-large')
 
     # Clean up
     bbox_props = dict(boxstyle="square", fc="lightgrey", ec="gray", lw=1.5)
-    plt.text(0.17, 0.82, "ECE: {:.4f}".format(ece), ha="center", va="center", size=20, weight = 'normal', bbox=bbox_props)
+    # ax.text(0.17, 0.82, "ECE: {:.4f}".format(ece), ha="center", va="center", size=20, weight = 'normal', bbox=bbox_props)
 
-    plt.title("Reliability Diagram", size=22)
-    plt.ylabel("Accuracy",  size=18)
-    plt.xlabel("Confidence",  size=18)
-    plt.xlim(0,1)
-    plt.ylim(0,1)
+    ax.set_title("Reliability Diagram - {}".format(model, size=20))
+    ax.set_ylabel("Accuracy",  size=18)
+    ax.set_xlabel("Confidence",  size=18)
+    ax.set_xlim(0,1)
+    ax.set_ylim(0,1)
     # plt.savefig('reliability_diagram.png')
-    return fig, ax
+    plt.show()
