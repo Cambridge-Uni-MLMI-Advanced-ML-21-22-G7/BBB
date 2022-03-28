db08d1d9aacdea98ec3ecdf2584687bcbe89caef
Max Bronckers <maxbronckers@me.com>
2022-03-28 15:23:36 +0100

    Adding adversarial plots; refactored evaluate_classifier & load_model components

---
+++ jupyter/model_details.py
@@ -0,0 +1,60 @@
+import os
+import json
+from glob import glob
+from collections import namedtuple
+
+import torch
+from bbb.models.dnn import ClassificationDNN
+from bbb.models.bnn import ClassificationBNN
+from bbb.config.parameters import Parameters, PriorParameters
+
+ModelDetails = namedtuple('ModelDetails', 'dir mclass description')
+
+MODEL_DETAILS_DICT = {
+    # BNN - Singe Gaussian Prior (300 epochs)
+    "bnn_sgp_1200": ModelDetails("../saved_models/BBB_classification/baseline/single_gaussian_prior/300_epochs/1200/2022-03-15-09.18.07", ClassificationBNN, "BNN - 1200 hidden units, SGP, 300 epochs"),
+    "bnn_sgp_800": ModelDetails("../saved_models/BBB_classification/baseline/single_gaussian_prior/300_epochs/800/2022-03-15-14.25.46", ClassificationBNN, "BNN - 800 hidden units, SGP, 300 epochs"),
+    "bnn_sgp_400": ModelDetails("../saved_models/BBB_classification/baseline/single_gaussian_prior/300_epochs/400/2022-03-15-14.26.34", ClassificationBNN, 
+    "BNN - 400 hidden units, SGP, 300 epochs"),
+
+    # BNN - Singe Gaussian Prior (600 epochs)
+    "bnn_sgp_600_1200": ModelDetails("../saved_models/BBB_classification/baseline/single_gaussian_prior/600_epochs/1200/2022-03-27-20.30.22", ClassificationBNN, "BNN - 1200 hidden units, SGP, 600 epochs"),
+
+    # BNN - MoG (600 epochs)
+    "bnn_mog_600_1200": ModelDetails("../saved_models/BBB_classification/baseline/mog_prior/sigma_1_1_sigma_2_exp_7/1200/600_epochs/2022-03-27-16.24.44", ClassificationBNN, "BNN - 1200 hidden units, MoG, 600 epochs"),
+    
+    # BNN - MoG (300 epochs)
+    "bnn_mog_1200": ModelDetails("../saved_models/BBB_classification/baseline/mog_prior/sigma_1_1_sigma_2_exp_7/1200/300_epochs/2022-03-27-07.54.00", ClassificationBNN, "BNN - 1200 hidden units, MoG, 300 epochs"),
+    "bnn_mog_800": ModelDetails("../saved_models/BBB_classification/baseline/mog_prior/sigma_1_1_sigma_2_exp_7/800/2022-03-27-10.10.21", ClassificationBNN, "BNN - 800 hidden units, MoG, 300 epochs"),
+    "bnn_mog_400": ModelDetails("../saved_models/BBB_classification/baseline/mog_prior/sigma_1_1_sigma_2_exp_7/400/2022-03-27-07.55.07", ClassificationBNN, 
+    "BNN - 400 hidden units, MoG, 300 epochs"),
+
+    # DNN - no dropout
+    "dnn_1200": ModelDetails("../saved_models/DNN_classification/baseline/1200/2022-03-15-14.28.25", ClassificationDNN, "DNN - 1200 hidden units"),
+    "dnn_800": ModelDetails("../saved_models/DNN_classification/baseline/800/2022-03-15-16.06.09", ClassificationDNN, "DNN - 800 hidden units"),
+    "dnn_400": ModelDetails("../saved_models/DNN_classification/baseline/400/2022-03-15-16.10.34", ClassificationDNN, "DNN - 400 hidden units"),
+    
+    # DNN - dropout
+    "dnn_do_400": ModelDetails("../saved_models/DNN_classification/dropout/0.5/1200/2022-03-15-15.21.46", ClassificationDNN, "Dropout - 400 hidden units, 0.5 dropout"),
+    "dnn_do_800": ModelDetails("../saved_models/DNN_classification/dropout/0.5/800/2022-03-15-15.58.04", ClassificationDNN, "Dropout - 800 hidden units, 0.5 dropout"),
+    "dnn_do_1200": ModelDetails("../saved_models/DNN_classification/dropout/0.5/400/2022-03-15-16.26.18", ClassificationDNN, "Dropout - 1200 hidden units, 0.5 dropout"),
+}
+
+def load_model(MODEL):
+    MODEL_DETAILS = MODEL_DETAILS_DICT[MODEL]
+    
+    with open(os.path.join(MODEL_DETAILS.dir, 'params.txt'), 'r') as f:
+        params_dict = json.load(f)
+
+    # Need to deserialise the prior_params into a PriorParameters object
+    if params_dict['prior_params']:
+        params_dict['prior_params'] = PriorParameters(**params_dict['prior_params'])
+
+    params = Parameters(**params_dict)
+    
+    # Load model
+    # net = MODEL_DETAILS.mclass(params=params, eval_mode=True).to(DEVICE)
+    net = MODEL_DETAILS.mclass(params=params, eval_mode=True)
+    net.model.load_state_dict(torch.load(os.path.join(MODEL_DETAILS.dir, 'model.pt'), map_location=torch.device('cpu')))
+
+    return net, params
